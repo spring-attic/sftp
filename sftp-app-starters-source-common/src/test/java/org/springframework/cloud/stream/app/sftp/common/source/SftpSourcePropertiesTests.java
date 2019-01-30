408dd870b5baf3ddb694990a419ac45546276007
Gary Russell <grussell@pivotal.io>
2019-01-30 17:28:13 -0500

    GH-42 privateKey and passphrase properties ignored
    Fixes https://github.com/spring-cloud-stream-app-starters/sftp/issues/42
    
    The private key (String) property and passphrase property were not added to
    the session factory.
    
    Furthermore, the private key property was a `String` rather than a `Resource`.
    
    - Fix the property type
    - Configure the session factory properly
    - Add a test public/private key pair (from SI)
    - Configure the test server to validate the public key
    - Change one of the tests to use key validation

--- sftp-app-starters-source-common/src/test/java/org/springframework/cloud/stream/app/sftp/common/source/SftpSourcePropertiesTests.java
+++ sftp-app-starters-source-common/src/test/java/org/springframework/cloud/stream/app/sftp/common/source/SftpSourcePropertiesTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,8 +22,6 @@ import org.junit.Test;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.cloud.stream.app.sftp.common.source.SftpSourceProperties;
-import org.springframework.cloud.stream.app.sftp.common.source.SftpSourceSessionFactoryConfiguration;
 import org.springframework.cloud.stream.config.SpelExpressionConverterConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
